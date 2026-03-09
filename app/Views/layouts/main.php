0848ad368cadc5f440c48410a3b1dc95f8566122
steviebarrett <mail@steviebarrett.com>
2026-03-09 10:47:17 +0000

    Added leaflet.js

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -5,7 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= e($title ?? 'Recordings') ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/0b481d2098.js" crossorigin="anonymous"></script>
+
+    <!-- leaflet CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+        crossorigin=""/>
+
+  <script src="https://kit.fontawesome.com/0b481d2098.js" crossorigin="anonymous"></script>
+
+  <!-- leaflet JS -->
+  <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+          integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+          crossorigin=""></script>
+
 </head>
 <body>
 

5ae5fc6e72585807c5d035197553cae099710e74
GeorgeDylan <claymoarplays@gmail.com>
2026-03-31 12:42:17 +0000

    favicon

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= e($title ?? 'Recordings') ?></title>
+    <link rel="icon" type="image/png" href="<?= e(base_path('/assets/images/favicon.png')) ?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- leaflet CSS -->
